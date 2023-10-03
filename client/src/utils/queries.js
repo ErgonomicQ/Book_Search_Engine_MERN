dc1e48551e32eb8fbddefe739230e815f234bda3
ErgonomicQ <ergonomicQ@gmail.com>
2023-10-02 19:09:08 -0700

    Adding initial mutations and queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from '@apollo/client';
+//Find a user and their accociated information
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
